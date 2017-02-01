88e55fbdfd39c5df27a4b407454d9b1a47958c3f
Viktoria <viktoria.efstratiou@groundsure.com>
2017-02-01 16:36:12 +0000

    small changes

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,19 +1,12 @@
 class Hamming
-	def self.compute(str1,str2)
-		count = 0
-		if str1.size != str2.size
-			raise ArgumentError.new('Different sequence length')
-		else
-			str1.size.times do |l|
-				unless str1[l] == str2[l]
-					count += 1
-				end
-			end
-			return count
-		end
-	end
+  def self.compute(str1, str2)
+    count = 0
+    raise ArgumentError, 'Different sequence length' if str1.size != str2.size
+    str1.size.times { |l| count += 1 unless str1[l] == str2[l] }
+    count
+  end
 end
 
 module BookKeeping
-	VERSION = 3
+  VERSION = 3
 end
