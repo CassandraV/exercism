f44ba5e501a56d59fed9817582a59182d6b02ecf
Viktoria <viktoria.efstratiou@groundsure.com>
2017-04-07 10:48:18 +0100

    bob improved

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,13 +1,29 @@
 class Bob
   def self.hey(phrase)
-    if phrase.contain(/[:word:]+/)
-      return 'Fine. Be that way!'
-    elsif phrase == phrase.upcase
-      return 'Whoa, chill out!'
-    elsif phrase.split("").last == "?"
-      return 'Sure.'
+    if uppercase?(phrase)
+      'Whoa, chill out!'
+    elsif question?(phrase)
+      'Sure.'
+    elsif !contains_text?(phrase) && !contains_number?(phrase)
+      'Fine. Be that way!'
     else
-      return 'Whatever.'
+      'Whatever.'
     end
+  end
+
+  def self.uppercase?(phrase)
+    phrase == phrase.upcase && contais_text?(phrase)
+  end
+
+  def self.question?(phrase)
+    phrase.split('').last == '?'
+  end
+
+  def self.contains_text?(phrase)
+    phrase.match(/[a-zA-Z]+/)
+  end
+
+  def self.contains_number?(phrase)
+    phrase.match(/[0-9]+/)
   end
 end
