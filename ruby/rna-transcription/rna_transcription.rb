6164ae12d68a48b21f17da109c1477d77ae1c20d
Viktoria <viktoria.efstratiou@groundsure.com>
2017-04-07 10:54:32 +0100

    improvments

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,12 +1,11 @@
 class Complement
   def self.of_dna(str)
-    dna = {'G'=> 'C', 'C'=>'G','T'=> 'A', 'A'=> 'U'}
+    dna = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
     ac_let = /[ACGT]/
-    str.match(/^[ACGT]*$/) ? str.gsub!(ac_let, dna) :  ""
+    str =~ /^[ACGT]*$/ ? str.gsub!(ac_let, dna) : ''
   end
-end		
-
-module BookKeeping
-	VERSION = 4
 end
 
+module BookKeeping
+  VERSION = 4
+end
